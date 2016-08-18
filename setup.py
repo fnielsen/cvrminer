58b92617e03742658a6362f66664109de8993038
fnielsen <fn@imm.dtu.dk>
2016-08-18 16:05:42 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cvrminer',
+    author='Finn Aarup Nielsen',
+    author_email='faan@dtu.dk',
+    license='Apache License',
+    url='https://github.com/fnielsen/cvrminer',
+    packages=['cvrminer'],
+    test_requires=['flake8'],
+    )
+  
