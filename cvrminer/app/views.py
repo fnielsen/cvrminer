ac012e66af60f744211a6f801f89551655e2e811
fnielsen <fn@imm.dtu.dk>
2016-10-19 18:55:06 +0200

    Create views.py

---
+++ cvrminer/app/views.py
@@ -0,0 +1,20 @@
+"""Views for cvrminer app."""
+
+from flask import render_template
+
+from . import app
+
+
+@app.route("/")
+def index():
+    """Return index page of for app."""
+    return render_template('base.html')
+
+
+@app.route("/smiley/")
+def smiley():
+    """Return smiley page of for app."""
+    table = app.smiley.db.tables.smiley.head(n=10000).to_html()
+    return render_template('smiley.html', table=table)
+
+
