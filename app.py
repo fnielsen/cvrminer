51a410a83dcfd271712c3e31dce5befc80f81b87
fnielsen <fn@imm.dtu.dk>
2016-11-29 10:55:38 +0100

    Change app to factory

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 """Entrypoint to start app."""
 
 
-from cvrminer.app import app
+from cvrminer.app import create_app
 
 
+app = create_app()
+    
 if __name__ == '__main__':
     app.run(debug=True)
