5b85b811428e9b81df70f966e5a08fb09b6872bc
fnielsen <fn@imm.dtu.dk>
2016-11-29 10:55:29 +0100

    Change app to factory

--- cvrminer/app/__init__.py
+++ cvrminer/app/__init__.py
@@ -6,12 +6,29 @@ from __future__ import absolute_import, division, print_function
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-from ..smiley import Smiley
 
+def create_app(smiley=False):
+    """Create app.
 
-app = Flask(__name__)
-Bootstrap(app)
+    Factory for app.
 
-app.smiley = Smiley()
+    Parameters
+    ----------
+    smiley : bool, optional
+        Determines whether the smiley functionality should be setup.
 
-from . import views
+    """
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    if smiley:
+        from ..smiley import Smiley
+
+        app.smiley = Smiley()
+    else:
+        app.smiley = None
+
+    from .views import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
