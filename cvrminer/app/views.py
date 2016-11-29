d1f21f6a4aa8f2d09a4b4a1ece341678f2f7182a
fnielsen <fn@imm.dtu.dk>
2016-11-29 10:55:45 +0100

    Change app to factory

--- cvrminer/app/views.py
+++ cvrminer/app/views.py
@@ -1,28 +1,33 @@
 """Views for cvrminer app."""
 
-from flask import render_template
+from flask import Blueprint, current_app, render_template
 
-from . import app
 from ..xbrler import search_for_regnskaber
 from ..wikidata import cvr_to_q
 
 
-@app.route("/")
+main = Blueprint('app', __name__)
+
+
+@main.route("/")
 def index():
     """Return index page of for app."""
     return render_template('base.html')
 
 
-@app.route("/smiley/")
+@main.route("/smiley/")
 def smiley():
     """Return smiley page of for app."""
-    table = app.smiley.db.tables.smiley.head(n=10000).to_html()
+    if current_app.smiley:
+        table = current_app.smiley.db.tables.smiley.head(n=10000).to_html()
+    else:
+        table = ''
     return render_template('smiley.html', table=table)
 
 
-@app.route("/cvr/<int:cvr>")
+@main.route("/cvr/<int:cvr>")
 def show_cvr(cvr):
-    """Return smiley page of for app."""
+    """Return CVR page of for app."""
     q = cvr_to_q(cvr)
     regnskaber = search_for_regnskaber(cvr=cvr)
     return render_template('cvr.html',
