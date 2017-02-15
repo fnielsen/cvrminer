69315fcb160c4bed11c9fca02fe0451902017dc8
fnielsen <fn@imm.dtu.dk>
2017-02-15 17:07:24 +0100

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os
 from setuptools import setup
+
+
+filename = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+requirements = open(filename).read().splitlines()
+
 
 setup(
     name='cvrminer',
     author='Finn Aarup Nielsen',
     author_email='faan@dtu.dk',
     license='Apache License',
+    install_requires=requirements,
     url='https://github.com/fnielsen/cvrminer',
     packages=['cvrminer'],
-    tests_require=['flake8'],
+    tests_require=['flake8', 'pydocstyle'],
 )
