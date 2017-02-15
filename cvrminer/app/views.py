d182105ffa215a43e1b68c104b13b5f36dcc3753
fnielsen <fn@imm.dtu.dk>
2017-02-15 16:35:48 +0100

    Implement views for branch

--- cvrminer/app/views.py
+++ cvrminer/app/views.py
@@ -2,11 +2,49 @@
 
 from flask import Blueprint, current_app, render_template
 
+from werkzeug.routing import BaseConverter
+
 from ..xbrler import search_for_regnskaber
 from ..wikidata import cvr_to_q
 
 
+class RegexConverter(BaseConverter):
+    """Converter for regular expression routes.
+
+    References
+    ----------
+    https://stackoverflow.com/questions/5870188
+
+    """
+
+    def __init__(self, url_map, *items):
+        """Setup regular expression matcher."""
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+def add_app_url_map_converter(self, func, name=None):
+    """Register a custom URL map converters, available application wide.
+
+    References
+    ----------
+    https://coderwall.com/p/gnafxa/adding-custom-url-map-converters-to-flask-blueprint-objects
+
+    """
+    def register_converter(state):
+        state.app.url_map.converters[name or func.__name__] = func
+
+    self.record_once(register_converter)
+
+
+
+Blueprint.add_app_url_map_converter = add_app_url_map_converter
 main = Blueprint('app', __name__)
+main.add_app_url_map_converter(RegexConverter, 'regex')
+
+
+# Wikidata item identifier matcher
+q_pattern = '<regex("Q[1-9]\d*"):q>'
 
 
 @main.route("/")
@@ -32,3 +70,34 @@ def show_cvr(cvr):
     regnskaber = search_for_regnskaber(cvr=cvr)
     return render_template('cvr.html',
                            cvr=cvr, regnskaber=regnskaber, q=q)
+
+
+@main.route('/branch/' + q_pattern)
+def show_branche(q):
+    """Return HTML rendering for specific branch.
+
+    Parameters
+    ----------
+    q : str
+        Wikidata item identifier.
+
+    Returns
+    -------
+    html : str
+        Rendered HTML.
+
+    """
+    return render_template('branch.html', q=q)
+
+
+@main.route('/branch/')
+def show_work_empty():
+    """Return rendered index page for branch.
+
+    Returns
+    -------
+    html : str
+        Rendered HTML page for branch index page.
+
+    """
+    return render_template('branch_empty.html')
