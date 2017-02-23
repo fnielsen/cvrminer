abc4c8fc2bbfeaefc2b5b64824adf0460ecafe5e
fnielsen <fn@imm.dtu.dk>
2017-02-23 17:43:11 +0100

    Change implementation of stop words handling

--- cvrminer/text.py
+++ cvrminer/text.py
@@ -1,24 +1,42 @@
-u"""text.
+"""text.
 
 Usage:
-  text clean-purpose <purpose>
+  text clean-purpose [options] <purpose>
 
-Example:
-  $ python -m cvrminer.text clean-purpose "Selskabets formål er handel"
-  handel
+Options:
+  --debug             Debug messages.
+  -h --help           Help message
+  --ie=encoding     Input encoding [default: utf-8]
+  --oe=encoding       Output encoding [default: utf-8]
+  -o --output=<file>  Output filename, default output to stdout
+  --verbose           Verbose messages.
 
 """
 
+
+import codecs
+
+import logging
+
+import os
+from os import write
+from os.path import join, split
+
 import re
 
-from six import u
+import signal
+
+from six import b, text_type
 
 
 class PurposeProcessor(object):
     """Text processor for company purpose."""
 
     def __init__(self):
-        """Setup pattern."""
+        """Setup pattern and logger."""
+        self.logger = logging.getLogger(__name__ + '.PurposeProcessor')
+        self.logger.addHandler(logging.NullHandler())
+
         self.ignore_pattern = re.compile(
             u"""
             Selskabets\sform\xe5l\ser\sat\sdrive|
@@ -28,38 +46,63 @@ class PurposeProcessor(object):
             """,
             flags=re.UNICODE | re.VERBOSE)
 
-        self.stop_words = set(re.split('\s+', u("""
-            af anden at
-            besl\xe6gtet
-            dermed efter forbindelse herunder i inden og om
-            samt til virksomhed
-        """).strip(), flags=re.UNICODE | re.DOTALL))
-
-        regex = '|'.join(self.stop_words)
+        self.stop_words = self.read_stop_words()
+        stop_words = sorted(self.stop_words, key=len, reverse=True)
+        regex = '|'.join((re.escape(word) for word in stop_words))
         regex = r'\b(?:' + regex + r')\b'
         self.stop_words_pattern = re.compile(regex, flags=re.UNICODE)
 
     def clean(self, text):
-        u"""Clean purpose text.
+        """Return cleaned purpose text.
 
         Parameters
         ----------
         text : str
            String with purpose
 
+        Returns
+        -------
+        cleaned : str
+           String with cleaned purpose
+
         Description
         -----------
         Remove the following fragments:
 
-        "Selskabets formal er at drive"
-        "Selskabets formål er"
-        "og anden dermed beslægtet virksomhed"
-        "dermed beslægtet virksomhed"
-        "hermed beslægtet virksomhed"
+        "Selskabets formaal er at drive"
+        "Selskabets formaal er"
+        "og anden dermed beslaegtet virksomhed"
+        "dermed beslaegtet virksomhed"
+        "hermed beslaegtet virksomhed"
 
         """
-        return self.stop_words_pattern.sub(
-            '', self.ignore_pattern.sub('', text)).strip()
+        cleaned = self.stop_words_pattern.sub('', text).strip()
+        return cleaned
+
+    def read_stop_words(self):
+        """Read purpose stop words from data file.
+
+        Returns
+        -------
+        stop_words : list of str
+            List with stop words
+
+        Examples
+        --------
+        >>> purpose_processor = PurposeProcessor()
+        >>> stop_words = purpose_processor.read_stop_words()
+        >>> 'dermed' in stop_words
+        True
+
+        """
+        filename = join(split(__file__)[0], 'data', 'purpose_stop_words.txt')
+        self.logger.info('Reading stop words from {}'.format(filename))
+
+        stop_words = []
+        with codecs.open(filename, encoding='utf-8') as fid:
+            for line in fid:
+                stop_words.append(line.strip())
+        return stop_words
 
 
 def main():
@@ -68,11 +111,40 @@ def main():
 
     arguments = docopt(__doc__)
 
+    logging_level = logging.WARN
+    if arguments['--debug']:
+        logging_level = logging.DEBUG
+    elif arguments['--verbose']:
+        logging_level = logging.INFO
+
+    logger = logging.getLogger()
+    logger.setLevel(logging_level)
+    logging_handler = logging.StreamHandler()
+    logging_handler.setLevel(logging_level)
+    logging_formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging_handler.setFormatter(logging_formatter)
+    logger.addHandler(logging_handler)
+
+    if arguments['--output']:
+        output_filename = arguments['--output']
+        output_file = os.open(output_filename, os.O_RDWR | os.O_CREAT)
+    else:
+        # stdout
+        output_file = 1
+    output_encoding = arguments['--oe']
+    input_encoding = arguments['--ie']
+
+    # Ignore broken pipe errors
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
     if arguments['clean-purpose']:
-        purpose = arguments['<purpose>'].decode('utf-8')
+        purpose = arguments['<purpose>']
+        if not isinstance(purpose, text_type):
+            purpose = purpose.decode(input_encoding)
         processor = PurposeProcessor()
         cleaned_purpose = processor.clean(purpose)
-        print(cleaned_purpose)
+        write(output_file, cleaned_purpose.encode(output_encoding) + b('\n'))
 
 
 if __name__ == '__main__':
